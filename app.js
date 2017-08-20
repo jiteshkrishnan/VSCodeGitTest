519dc5bc652904c2457832da4b65cb8aef64115e
jiteshkrishnan <jiteshkrishnan@gmail.com>
2017-08-20 23:01:19 +0530

    new react sample app

---
+++ app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class MyApp extends Component {
+    render() {
+        return (
+            <div>
+               Welcome to React sample demo 
+            </div>
+        );
+    }
+}
