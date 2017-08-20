633e26ebc5020220036ade4f263fe2be4090949d
jiteshkrishnan <jiteshkrishnan@gmail.com>
2017-08-21 00:07:23 +0530

    added console.log

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import main from './main.jsx'
 
 export default class MyApp extends Component {
+    constructor(props){
+        super(props)
+        console.log(props)
+    }
     render() {
         return (
             <div>
